ab9b1cb40f9dc8ab135768fdc2af6e671b67eb14
Chinmay Kulkarni <chnm.kulkarni@gmail.com>
2018-04-08 19:04:19 -0600

    Update number of hugepages for Sandstorm.

--- sandstorm_setup.sh
+++ sandstorm_setup.sh
@@ -34,7 +34,7 @@ echo ssh > /etc/pdsh/rcmd_default
 # Enable hugepage support: http://dpdk.org/doc/guides/linux_gsg/sys_reqs.html
 # The changes will take effects after reboot.
 # Reserve 1GB hugepages via kernel boot parameters
-kernel_boot_params="default_hugepagesz=1G hugepagesz=1G hugepages=4"
+kernel_boot_params="default_hugepagesz=1G hugepagesz=1G hugepages=16"
 
 # Disable intel_idle driver to gain control over C-states (this driver will
 # most ignore any other BIOS setting and kernel parameters). Then limit
